75adcde57a26844df7bf6975ba887eccbcec2e95
loganschmitt14 <loganschmitt14@gmail.com>
2024-02-17 18:40:32 -0500

    Create GraphQL Schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { WelcomePage, GitHubBanner, Refine } from "@refinedev/core";
+import {
+  WelcomePage,
+  GitHubBanner,
+  Refine,
+  Authenticated,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
+import { Home, ForgotPassword, Login, Register } from "./pages";
+
 import { dataProvider, liveProvider } from "./providers";
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { authProvider } from "./providers";
+import Layout from "./components/layout";
 
 function App() {
   return (
@@ -37,7 +46,24 @@ function App() {
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<Home />} />
+                </Route>
               </Routes>
 
               <RefineKbar />
